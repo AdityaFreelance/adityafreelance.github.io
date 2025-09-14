4610153783adbbf1ab859fa263365e18b89a1cb0
Aditya2376 <109151812+Aditya2376@users.noreply.github.com>
2025-09-14 21:07:35 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// This script initializes the AOS library, which provides animations on scroll.
+
+AOS.init({
+    // The duration of the animation in milliseconds.
+    duration: 1000,
+    // The easing function to use for the animation.
+    easing: 'ease-in-out',
+    // Whether the animation should only happen once.
+    once: true,
+});
